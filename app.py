908d37e7a8ba8aecb4789df9013b249dfbee5084
james.sullivan <james.sullivan@axomic.com>
2023-10-04 16:53:55 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+from supabase import create_client
+import stripe
+import os
+import json
+
+from supabase import create_client, Client
+
+from flask import Flask, request
+
+app = Flask(__name__)
+SUPABASE_PROJECT_URL: str = 'https://jrxlluxajfavygujjygc.supabase.co'
+url = os.getenv('SUPABASE_PROJECT_URL')
+key = os.getenv('SUPABASE_API_KEY')
+SUPABASE_API_KEY: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyeGxsdXhhamZhdnlndWpqeWdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzNzU0MjAsImV4cCI6MjAxMTk1MTQyMH0.PqkAMN8KFACclum_-86xMSzphxKXUSU26QL5Oi-iQFE'
+supabase: Client = create_client(SUPABASE_PROJECT_URL, SUPABASE_API_KEY)
+
+@app.route('/')
+def default():
+    return "Hello World"
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    print(data['email'])
+    user = supabase.auth.sign_in(email=data['email'], password=data['password'])
+    print(user)
+    return "logged in"
+
+@app.route('/supabase/create')
+def create():
+    return "Supabase CREATE"
+
+@app.route('/get')
+def get():
+    data = supabase.table('Products').select("*").execute()
+    return data.data
+
+@app.route('/supabase/update')
+def update():
+    return "Supabase UPDATE"
+
+@app.route('/supabase/delete')
+def delete():
+    return "Supabase DELETE"
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host='0.0.0.0',port=5000)
